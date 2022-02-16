623ffb0125f055ce8c0a273efe17a6bfe705c856
omarsamak <omarhesham83@gmail.com>
2022-02-16 17:04:44 +0200

    feat: setup a simple server

--- build/index.js
+++ build/index.js
@@ -1,1 +1,14 @@
-'use strict'
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 3000;
+app.get('/api', function (req, res) {
+    res.send('<h1>Welcome Home</h1>');
+});
+app.listen(port, function () {
+    console.log("server started at localhost:".concat(port));
+});
